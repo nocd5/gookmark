11ca17e40085cedc6a77ad7ec34d462636f1f466
nocd5 <nocd5rd@gmail.com>
2015-04-24 00:27:30 +0900

    Tentatively implementation alias

--- gookmark.go
+++ gookmark.go
@@ -1,10 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/codegangsta/cli"
 )
+
+func Contains(s []string, e string) bool {
+	for _, _s := range s {
+		if _s == e {
+			return true
+		}
+	}
+	return false
+}
+
+func ReplaceArgsByAlias(src []string) []string {
+	var dest []string
+	var subcommands []string
+
+	config, err := LoadConfig()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return src
+	}
+
+	for _, cmd := range Commands {
+		subcommands = append(subcommands, cmd.Name)
+	}
+
+	skipNext := false
+	for _, arg := range src {
+		if skipNext {
+			dest = append(dest, arg)
+			skipNext = false
+			continue
+		}
+
+		if Contains(subcommands, arg) {
+			skipNext = true
+		}
+
+		alias := config.Alias[arg]
+		if len(alias) > 0 {
+			dest = append(dest, strings.SplitN(alias, " ", 2)...)
+		} else {
+			dest = append(dest, arg)
+		}
+	}
+
+	return dest
+}
 
 func main() {
 	app := cli.NewApp()
@@ -15,5 +63,5 @@ func main() {
 	app.Email = "nocd5rd@gmail.com"
 	app.Commands = Commands
 
-	app.Run(os.Args)
+	app.Run(ReplaceArgsByAlias(os.Args))
 }
